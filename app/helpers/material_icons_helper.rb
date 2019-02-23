0a8cfffd650035d289846fe1d49eb0f5fefe586d
AquisTech <anand.bait@gmail.com>
2019-02-23 16:51:36 +0530

    Fix for multiple classes passed to icon_tag

--- app/helpers/material_icons_helper.rb
+++ app/helpers/material_icons_helper.rb
@@ -1,6 +1,6 @@
 module MaterialIconsHelper
   def icon_tag(content, classes = '')
-    classes += 'material-icons'
+    classes += ' material-icons'
     content_tag(:i, content, class: classes)
   end
 end
